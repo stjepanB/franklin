568e02c5a4a2e28eed4c8363dc52ce5cbbe5326e
Stjepan Bencic <Stjepan.Bencic@ibm.com>
2023-05-08 15:40:23 +0200

    asda

--- blocks/hero/hero.js
+++
@@ -1,9 +0,0 @@
-import { createOptimizedPicture } from '../../scripts/lib-franklin.js';
-
-export default function decorate(block) {
-  /* change to ul, li */
-  if(block.textContent.startsWith("Frank")){
-    return;
-  }
-  block.textContent = block.textContent + '   FIRST JS CODE -> Hello World';
-}
