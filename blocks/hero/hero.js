671b55250337c901b38d8b3b7d48fdc7787691fb
Stjepan Bencic <Stjepan.Bencic@ibm.com>
2023-05-08 15:33:04 +0200

    js chnage

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -2,5 +2,5 @@ import { createOptimizedPicture } from '../../scripts/lib-franklin.js';
 
 export default function decorate(block) {
   /* change to ul, li */
-  block.textContent = 'FIRST JS CODE -> Hello World';
+  block.textContent = block.textContent + '   FIRST JS CODE -> Hello World';
 }
